da470092f8205dc6ae2de37412f078bbf3e41383
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-08-30 13:38:56 +0300

    to remove unnecessary warning of eslint, eslint-disable comment used

--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -3,6 +3,7 @@ export const textValidation = (text, minTextLength = 1) => {
 };
 
 export const emailValidation = (email) => {
+  // eslint-disable-next-line
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   return regex.test(email);
